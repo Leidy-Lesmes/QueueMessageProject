4e87bd9fd9575fb6a4c2c9cbc5a807e6506734c5
Catalina Fajardo <tania.fajardo01@uptc.edu.co>
2024-05-26 18:23:10 -0500

    cambio a http

--- queue.js
+++ queue.js
@@ -1,77 +1,75 @@
-require('dotenv').config();
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
 const axios = require('axios');
-
+const express = require('express');
 const app = express();
+const port = 4000;
+
 app.use(express.json());
-const server = http.createServer(app);
 
-const connectedClients = [];
+let requestQueue = [];
+let instances = [
+    'http://localhost:3000',
+    'http://localhost:3001'
+];
 
-const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST'],
-        allowedHeaders: ['Content-Type'],
-        credentials: true
-    }
-});
-
-io.on('connection', (socket) => {
-    const origin = socket.handshake.query.clientUrl || 'unknown';
-    console.log(`Nuevo cliente conectado desde: ${origin}`);
-
-    connectedClients.push(origin);
-
-    socket.on('disconnect', () => {
-        console.log(`Cliente ${origin} desconectado.`);
-        
-        const index = connectedClients.indexOf(origin);
-        if (index !== -1) {
-            connectedClients.splice(index, 1);
-        }
-    });
-});
-
-// Array para almacenar los trabajos en la cola
-const messageQueue = [];
-const MAX_JOBS_PER_INSTANCE = 5;
-
-// Endpoint para obtener la lista de URLs de los clientes conectados
-app.get('/connected-clients', (req, res) => {
-    res.json(connectedClients);
-});
-
-// Endpoint para recibir datos
-app.post('/add-job', async (req, res) => {
+app.post('/add-to-queue', (req, res) => {
     const { keyword, email } = req.body;
 
-    // Imprimir los valores recibidos en la consola
-    console.log('Keyword recibida:', keyword);
-    console.log('Email recibido:', email);
-
-    // Validar que ambos datos están presentes
     if (!keyword || !email) {
-        return res.status(400).send('La palabra clave y el correo electronico son obligatorios');
+        return res.status(400).send('Keyword and email are required');
     }
+    requestQueue.push({ keyword, email });
+    console.log('Request Queue:', requestQueue);
 
-    // Agregar el trabajo a la cola
-    messageQueue.push({ keyword, email });
-
-     // Emitir evento para enviar datos al consumidor
-     io.emit('new-job', { keyword, email });
-
-     console.log('Trabajo enviado al consumidor:');
-     console.log('Keyword:', keyword);
-     console.log('Email:', email);
-     
-    res.send('Peticion agregada a la cola');
+    res.send('Request added to queue');
 });
 
+// Función para procesar la cola de solicitudes
+function processQueue() {
+    if (requestQueue.length === 0) {
+        return;
+    }
 
-const port = process.env.PORT || 4000;
-server.listen(port, () => {
-    console.log(`Servidor HTTP escuchando en el puerto ${port}`);
+    let instanceIndex = 0;
+
+    for (let i = 0; i < requestQueue.length; i++) {
+        const request = requestQueue[i];
+        const instance = instances[instanceIndex];
+
+        axios.get(`${instance}/ping`)
+            .then(response => {
+                console.log(`Response from ${instance}:`, response.data);
+                if (response.data.isBusy === false) {
+                    console.log(`Sending request to instance ${instance}:`, request);
+                    sendRequestToInstance(instance, request);
+                    requestQueue.splice(i, 1);
+                    return;
+                };
+            })
+            .catch(error => {
+                console.error(`Error pinging instance ${instance}:`, error.message);
+            });
+        console.log('Request Queue:', requestQueue);
+        instanceIndex = (instanceIndex + 1) % instances.length;
+    }
+}
+
+// Función para enviar la solicitud a la instancia
+function sendRequestToInstance(instance, { keyword, email }) {
+    axios.post(`${instance}/scrape-and-send`, { keyword, email })
+        .then(response => {
+            console.log(`Request processed by ${instance}:`, response.data);
+        })
+        .catch(error => {
+            console.error(`Error processing request with ${instance}:`, error.message);
+        })
+        .finally(() => {
+            processQueue();
+        });
+}
+
+// Iniciar el intervalo para realizar ping cada 4 segundos
+setInterval(processQueue, 4000);
+
+app.listen(port, () => {
+    console.log(`Queue manager running at http://localhost:${port}/`);
 });
